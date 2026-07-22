495359d619afcaac7ab1cc711acd4245c69e7fd5
Simon Nakarmi <simonnkm@gmail.com>
2026-07-22 14:26:59 -0700

    configure prisma postgres for vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,22 @@
 import { PrismaClient } from "./generated/prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-const getPrisma = () => {
-  const dbUrl = process.env.DATABASE_URL || "file:./dev.db";
-  const adapter = new PrismaBetterSqlite3({ url: dbUrl });
-  return new PrismaClient({ adapter });
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
 };
 
-export const prisma = globalForPrisma.prisma || getPrisma();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
+
+export default prisma;
